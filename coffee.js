c84c7a4697328a596fa8d00533f41b9cf3ccb9db
VictorSully79 <victorsullivan@hotmail.com>
2021-01-12 20:15:52 -0800

    fixed spacing

--- coffee.js
+++ coffee.js
@@ -11,18 +11,18 @@ var male = prompt('Am I a Male? Please answer YES or NO ');
 var male = male.toUpperCase();
 console.log('Am I a Male ' + male); {
   if (male == 'YES' || male == 'Y') {
-  alert('Correct, your smarter than I thought ' + nameq );
+    alert('Correct, your smarter than I thought ' + nameq );
 } else if (male != 'YES' || male != 'Y') 
-  alert('Wrong! I know your smarter than that, ' + nameq); 
+    alert('Wrong! I know your smarter than that, ' + nameq); 
 }
 
 var age = prompt('Am I over the age of 40? Please answer YES or NO');
 var age = age.toUpperCase();
 console.log('User thinks I am over 40 ' + age); {
   if (age == 'YES' || age == 'Y') {
-  alert("Correct, but your reply is still insulting");
+    alert("Correct, but your reply is still insulting");
 } else if (age != 'YES' || age != 'Y')
-  alert(" You're nice but wrong, I knew we were friends!");
+    alert(" You're nice but wrong, I knew we were friends!");
 }
 
 var location1 = prompt('Do I live in Washington State? YES or NO ');
@@ -30,16 +30,16 @@ var location1 = location1.toUpperCase();
 console.log('Do I live in Washington State? ' + location1); {
   if (location1 == 'YES' || location1 == 'Y') {
     alert('Correct, but its a little creepy.');
-  } else if (location1 != 'YES' || location1 != 'Y')
+} else if (location1 != 'YES' || location1 != 'Y')
     alert('Wrong, but at least youre not stalking me.');
-  }
+}
 
 var dogs = prompt('Do I have a dog? YES or NO ');
 var dogs = dogs.toUpperCase();
 console.log('Do I have dogs ' + dogs); {
   if (dogs == 'YES' || dogs == 'Y') {
     alert('I do, actually 2');
-   } else if (dogs != 'YES' || dogs != 'Y')
+} else if (dogs != 'YES' || dogs != 'Y')
      alert('Wrong, but we can still be friends. ');
 }
 
@@ -47,8 +47,8 @@ var army = prompt('Was I in the Army? ');
 var army = army.toUpperCase();
 console.log('Was I in the Army ' + army); {
   if (army == "YES" || army == 'Y') {
-    alert('Sure was, and man it sucks!');
+    alert('Sure was, ' + nameq +  ' man it sucked!');
 } else if(army != 'YES' || army !='Y') {
-    alert('Wrong! ');
+    alert('Wrong! ' + nameq);
 }
 }
