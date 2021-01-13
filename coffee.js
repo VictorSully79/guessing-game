93153270f8267728bcb41bf75b07bafa4df094b5
VictorSully79 <victorsullivan@hotmail.com>
2021-01-12 17:11:21 -0800

    Added initial files and started work on project

---
+++ coffee.js
@@ -0,0 +1,54 @@
+ 
+ 'use strict' 
+ var nameq = prompt('To whome am I speaking? ');
+ var responseName = 'Nice to meet you ' ;
+ alert(responseName + nameq);
+ console.log('The users name is ' + nameq); 
+ 
+document.write("Welcome " + nameq); 
+
+var male = prompt('Am I a Male? Please answer YES or NO ');
+var male = male.toUpperCase();
+console.log('Am I a Male ' + male); {
+  if (male == 'YES' || male == 'Y') {
+  alert('Correct, your smarter than I thought ' + nameq );
+} else if (male != 'YES' || male != 'Y') 
+  alert('Wrong! I know your smarter than that, ' + nameq); 
+}
+
+var age = prompt('Am I over the age of 40? Please answer YES or NO');
+var age = age.toUpperCase();
+console.log('User thinks I am over 40 ' + age); {
+  if (age == 'YES' || age == 'Y') {
+  alert("Correct, but your reply is still insulting");
+} else if (age != 'YES' || age != 'Y')
+  alert(" You're nice but wrong, I knew we were friends!");
+}
+
+var location1 = prompt('Do I live in Washington State? YES or NO ');
+var location1 = location1.toUpperCase();
+console.log('Do I live in Washington State? ' + location1); {
+  if (location1 == 'YES' || location1 == 'Y') {
+    alert('Correct, but its a little creepy.');
+  } else if (location1 != 'YES' || location1 != 'Y')
+    alert('Wrong, but at least youre not stalking me.');
+  }
+
+var dogs = prompt('Do I have a dog? YES or NO ');
+var dogs = dogs.toUpperCase();
+console.log('Do I have dogs ' + dogs); {
+  if (dogs == 'YES' || dogs == 'Y') {
+    alert('I do, actually 2');
+   } else if (dogs != 'YES' || dogs != 'Y')
+     alert('Wrong, but we can still be friends. ');
+}
+
+var army = prompt('Was I in the Army? ');
+var army = army.toUpperCase();
+console.log('Was I in the Army ' + army); {
+  if (army == "YES" || army == 'Y') {
+    alert('Sure was, and man it sucks!');
+} else if(army != 'YES' || army !='Y') {
+    alert('Wrong! ');
+}
+}
